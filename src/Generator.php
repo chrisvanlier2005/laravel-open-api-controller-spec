d2277987f5de81b65bae925ba17c455001d9f742
Chris van Lier <93382176+chrisvanlier2005@users.noreply.github.com>
2024-02-06 18:58:09 +0100

    wip

--- src/Generator.php
+++ src/Generator.php
@@ -40,8 +40,7 @@ class Generator
     {
         $this->setTraverser();
 
-
-
+        return [];
     }
 
     /**
--- src/GeneratorOptions.php
+++ src/GeneratorOptions.php
@@ -4,5 +4,16 @@ namespace ChrisVanLier2005\OpenApiGenerator;
 
 class GeneratorOptions
 {
-
+    /**
+     * Create a new instance of the GeneratorOptions.
+     *
+     * @param array<array-key, class-string> $only Only use these classes for the generation.
+     * @param bool $useResourceRouting Use the resource name for generating endpoints.
+     * @return void
+     */
+    public function __construct(
+        public array $only = [],
+        public bool $useResourceRouting = true,
+    ) {
+    }
 }
